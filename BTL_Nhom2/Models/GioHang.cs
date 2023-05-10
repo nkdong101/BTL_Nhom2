3d3f01750493642b63aa11746100acdc268a7df2
Dong <gnodong2002@gmail.com>
2023-05-10 21:50:53 +0700

    dong commit

--- BTL_Nhom2/Models/GioHang.cs
+++ BTL_Nhom2/Models/GioHang.cs
@@ -25,7 +25,7 @@ namespace BTL_Nhom2.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ChiTietGioHang> ChiTietGioHangs { get; set; }
-
+        //this change
         public virtual TaiKhoan TaiKhoan { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
