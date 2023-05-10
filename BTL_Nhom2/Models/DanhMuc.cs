85ed07fd2073246cd7a338a4de101180af2bf4cd
Dong <gnodong2002@gmail.com>
2023-05-10 22:01:13 +0700

    Required data

--- BTL_Nhom2/Models/DanhMuc.cs
+++ BTL_Nhom2/Models/DanhMuc.cs
@@ -1,7 +1,8 @@
-namespace BTL_Nhom2.Models
+﻿namespace BTL_Nhom2.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -18,8 +19,9 @@ namespace BTL_Nhom2.Models
         [Key]
         public int MaDM { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Tên danh mục không được để trống!")]
         [StringLength(100)]
+        [DisplayName("Tên danh mục")]
         public string TenDM { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
